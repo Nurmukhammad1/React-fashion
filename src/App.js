ba7273a96465dd73aab8c07e20b8159df15674c9
Nurmukhammad1 <90842370+Nurmukhammad1@users.noreply.github.com>
2023-07-04 10:40:27 +0500

    добавление секций arrivals, brands, card, promo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react' ;
 
+import Arrivals from './components/arrivals/Arrivals';
+import Brands from './components/brands/Brands';
+import Header from './components/header/Header'
+import Promo from './components/promo/Promo';
+
+
+
 function App() {
   return (
     <div className='App'>
-      <h1>Fashion</h1>
+      <Header/>
+      <Promo/>
+      <Brands/>
+      <Arrivals/>
     </div>
   );
 }
